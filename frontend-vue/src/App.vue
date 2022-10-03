81a728a2327472de1ef9a6a3f31e71c910a076e8
Klapeks <sashaKaraguts@gmail.com>
2022-10-04 02:33:12 +0300

    First Test Vue Project

---
+++ frontend-vue/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+    <Posts />
+</template>
+
+
+<script lang="ts">
+import Posts from '@/components/Posts.vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'App',
+    components: {
+        Posts,
+    },
+});
+</script>
+
+
+<style lang="scss" scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ frontend-vue/src/components/Posts.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="posts">
+        <h4>Sussy count: {{sus}}</h4>
+        <button @click="addSusCount">Sus it</button>
+        <button @click="removeSusCount">No sus</button>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: "Posts",
+    data() {
+        return {
+            sus: 0
+        }
+    },
+    methods: {
+        addSusCount() {
+            this.sus += 1;
+        },
+        removeSusCount() {
+            this.sus -= 1;
+        }
+    }
+})
+</script>
